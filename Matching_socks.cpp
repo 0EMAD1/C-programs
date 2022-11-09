9829f0a28ac002cfbd7ec63923e8d160263a1759
0EMAD1 <117738874+0EMAD1@users.noreply.github.com>
2022-11-09 13:43:13 +0530

    Add files via upload

---
+++ Matching_socks.cpp
@@ -0,0 +1,27 @@
+//Question -YourBuddy is getting late for college. She is looking for a matching pair of socks from a box full of socks. 
+//In the worst-case scenario, 
+//how many socks should YourBuddy remove from her drawer until she finds a matching pair
+
+//code:
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+
+ int t;
+
+ cin>>t;
+
+ while(t--){
+
+   int n;
+
+   cin>>n;
+
+   cout<<n+1<<endl;
+
+ }
